65c922fb99a61a45630040b7544b219ae7928fbc
Ninhow <allamoolsson09@gmail.com>
2021-11-06 15:59:37 +0100

    Created first labb with maven

---
+++ labb_0/src/main/java/com/iot/app/App.java
@@ -0,0 +1,13 @@
+package com.iot.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
